608e5707c722a93aa148fef68befd29cfaf56573
mel-edo <mel-edo@users.noreply.github.com>
2026-07-02 15:32:06 +0000

    deploy: bcfb29cadb7826ebf774b77e801c901b5c73214b

--- searcher-09f2665d.js
+++ searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-037107b8.js',
+                path_to_root + 'searchindex-c259a9e4.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
